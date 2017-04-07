08dac10296695f6a693498310245444013df7e6c
Matheus Campos Cardoso <its.matheus3@gmail.com>
2017-04-07 16:19:45 +0000

    Fix qrcode image on production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,7 +21,7 @@ Rails.application.configure do
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
-  config.gem 'rmagick',    :lib => 'RMagick'
+  config.gem 'mini_magick',    :lib => 'MiniMagick'
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
