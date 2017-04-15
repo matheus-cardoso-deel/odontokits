ccbbe732af1b72560f8094690590ccfd829d4767
Matheus Cardoso <its.matheus3@gmail.com>
2017-04-15 07:57:35 +0000

    Adds eager loading to application.rb

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module OdontoKits
   class Application < Rails::Application
     config.autoload_paths << Rails.root.join('lib')
     config.time_zone = 'Brasilia'
+    config.eager_load_paths << Rails.root.join('lib')
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
