a7762132076d8e6a9ebbc5a0812fba0f2aab39a4
Matheus Campos Cardoso <its.matheus3@gmail.com>
2021-08-03 09:18:09 -0300

    Update database config

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20171030134139) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "alunos", force: :cascade do |t|
     t.string   "image_uid"
     t.string   "nome"
@@ -21,8 +24,8 @@ ActiveRecord::Schema.define(version: 20171030134139) do
     t.string   "email"
     t.string   "password_digest"
     t.string   "periodo"
-    t.index ["email"], name: "index_alunos_on_email", unique: true
-    t.index ["matricula"], name: "index_alunos_on_matricula", unique: true
+    t.index ["email"], name: "index_alunos_on_email", unique: true, using: :btree
+    t.index ["matricula"], name: "index_alunos_on_matricula", unique: true, using: :btree
   end
 
   create_table "feeds", force: :cascade do |t|
@@ -30,8 +33,8 @@ ActiveRecord::Schema.define(version: 20171030134139) do
     t.integer  "kit_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["kit_id", "created_at"], name: "index_feeds_on_kit_id_and_created_at"
-    t.index ["kit_id"], name: "index_feeds_on_kit_id"
+    t.index ["kit_id", "created_at"], name: "index_feeds_on_kit_id_and_created_at", using: :btree
+    t.index ["kit_id"], name: "index_feeds_on_kit_id", using: :btree
   end
 
   create_table "kit_types", force: :cascade do |t|
@@ -47,9 +50,9 @@ ActiveRecord::Schema.define(version: 20171030134139) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.integer  "kit_type_id"
-    t.index ["aluno_id", "created_at"], name: "index_kits_on_aluno_id_and_created_at"
-    t.index ["aluno_id"], name: "index_kits_on_aluno_id"
-    t.index ["kit_type_id"], name: "index_kits_on_kit_type_id"
+    t.index ["aluno_id", "created_at"], name: "index_kits_on_aluno_id_and_created_at", using: :btree
+    t.index ["aluno_id"], name: "index_kits_on_aluno_id", using: :btree
+    t.index ["kit_type_id"], name: "index_kits_on_kit_type_id", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
@@ -60,7 +63,10 @@ ActiveRecord::Schema.define(version: 20171030134139) do
     t.string   "password_digest"
     t.string   "remember_digest"
     t.boolean  "admin",           default: false
-    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
   end
 
+  add_foreign_key "feeds", "kits"
+  add_foreign_key "kits", "alunos"
+  add_foreign_key "kits", "kit_types"
 end
